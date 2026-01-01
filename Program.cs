854b7339d26a72670e54e02992b1fb603af31d5a
Abu raad <herohm98@gmail.com>
2026-01-01 16:40:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿ using System;
+
+namespace TEST
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== SpaceGym: Delet user  ===");
+            Console.WriteLine("-----------------------------------------------");
+
+
+
+        }
+    }
+
+}
